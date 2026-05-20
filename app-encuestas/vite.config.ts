18802ea1b3c6c8895a6b1a5ba67db1de81331955
ShadowCoque <joel.coque@epn.edu.ec>
2026-05-20 12:41:44 -0500

    Cambios en la IP para peticiones
    Cambios en la IP del backend

--- app-encuestas/vite.config.ts
+++ app-encuestas/vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://192.168.3.168:3000',
         changeOrigin: true,
       },
     },
